3c7d1e97b8b6410498b010c017ac19e7ed95ae9f
MykolaPinchuk <mykolapinchuk@gmail.com>
2023-04-24 14:33:05 +0000

    progressing on pipeline part2

---
+++ pipeline_run.sh
@@ -0,0 +1,17 @@
+# may need to create or load venv here
+source /opt/conda/bin/activate ~/conda_env/py38
+
+export BUCKET="mpg3-testflights-polished-vault-379315"
+export PROJECT_ID="polished-vault-379315"
+
+python ./main.py \
+    --input=gs://${BUCKET}/data/processed/2020/2020-05.csv \
+    --output-flights=gs://${BUCKET}/features/flight_features/ \
+    --output-airports=gs://${BUCKET}/features/airport_features/ \
+    --output-read-instances=gs://${BUCKET}/features/read_instances/ \
+    --runner=DataflowRunner \
+    --project=${PROJECT_ID} \
+    --region=us-central1 \
+    --staging_location=gs://${BUCKET}/beam_staging \
+    --temp_location=gs://${BUCKET}/beam_tmp \
+    --job_name=flight-batch-features 
